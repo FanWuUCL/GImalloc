6ec4b8ca67fad2f96847719e8d24445ecab02474
FanWuUCL <fan.wu.12@ucl.ac.uk>
2013-11-21 16:20:16 +0000

    Fix prepare typo

--- Experiments/exp1/prepare.sh
+++ Experiments/exp1/prepare.sh
@@ -1,6 +1,6 @@
 
 
 MILU="../../Milu/bin/milu"
-SUBJECT_PATH ="../../Subjects/fat_memory/bin/fat_memory
+SUBJECT_PATH ="../../Subjects/fat_memory/bin/fat_memory"
 
 $MILU -o mop.txt -f func_list.txt malloc_pp.c 

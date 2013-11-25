7e573c21aadab17775102b15f3b76d721f8b0f3f
FanWuUCL <fan.wu.12@ucl.ac.uk>
2013-11-25 18:54:20 +0000

    Set up experimental env for "hashmap" under Experiments/exp7.

---
+++ Experiments/exp7/prepare.sh
@@ -0,0 +1,59 @@
+CURR_PATH=`pwd`
+memory_path="$CURR_PATH/../../memory/bin/memory"
+SUBJECT_PATH="$CURR_PATH/../../Subjects"
+ORIGINAL_PATH="$CURR_PATH/original"
+
+MILU="../../Milu/bin/milu"
+SUBJECT_NAME="hashmap"
+SUBJECT_PATH_NAME="$SUBJECT_PATH/c_hashmap"
+SUBJECT_PATH_TESTCASES="$SUBJECT_PATH_NAME/testcases"
+
+
+echo "Experiment: mutation sensitivity analysis"
+echo "Subject: hashmap"
+echo "November 25, 2013"
+
+echo "Compiling original"
+
+cd $CURR_PATH
+if [ ! -f fitness.txt ]; then
+       touch fitness.txt 
+       else
+       rm fitness.txt
+       touch fitness.txt
+ fi
+
+cd $ORIGINAL_PATH 
+gcc -shared -fPIC malloc.c -o libmalloc.so
+cp $SUBJECT_PATH/bin/$SUBJECT_NAME subject
+cp -r $SUBJECT_PATH_TESTCASES/* .
+cp $memory_path memory
+./memory > fitness.txt
+cat *.s > results.txt
+echo "Original: " >> $CURR_PATH/fitness.txt
+cat fitness.txt >> $CURR_PATH/fitness.txt
+
+cd $CURR_PATH
+
+$MILU -m mop.txt -f func_list.txt malloc.c 
+
+for index in `ls milu_output/mutants/`
+do
+	cd milu_output/mutants/$index
+    echo "Compiling mutant $index"
+	gcc -shared -fPIC src/malloc.c -o libmalloc.so
+    cp $SUBJECT_PATH/bin/$SUBJECT_NAME subject
+    cp -r $SUBJECT_PATH_TESTCASES/* .
+	cp $memory_path memory
+	./memory > fitness.txt
+    cat *.s > results.txt
+    if diff results.txt $ORIGINAL_PATH/results.txt >/dev/null ; then
+          echo "tests: 1" >> fitness.txt
+          else
+                echo "tests: 0" >> fitness.txt 
+    fi
+    echo "Mutant: $index" >> $CURR_PATH/fitness.txt
+    cat fitness.txt >> $CURR_PATH/fitness.txt
+	cd ../../..
+
+done

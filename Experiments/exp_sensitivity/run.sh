946d2181fff468a1975b4e1f8e25f5bcaab3fbe5
Yue Jia <yueplus@googlemail.com>
2013-11-26 00:39:20 +0000

    The newly desgined experiment script
    A more gemeric framework

--- Experiments/exp_sensitivity/run.sh
+++ Experiments/exp_sensitivity/run.sh
@@ -3,6 +3,7 @@ echo "November 25, 2013"
 
 
 CURR_PATH=`pwd`
+RUN_EXP="false"
 
 if [ ! -f fitness.txt ]; then
        touch fitness.txt 
@@ -11,6 +12,38 @@ if [ ! -f fitness.txt ]; then
        touch fitness.txt
  fi
 
-cd espresso
-./run.sh
+if [ "$1" == "espresso" ]; then
+
+SUBJECT_NAME="espresso"
+SUBJECT_PATH_NAME="espresso-ab-1.0"
+RUN_EXP="true"
+
+fi
+
+if [ "$1" == "cfrac" ]; then
+
+SUBJECT_NAME="cfrac"
+SUBJECT_PATH_NAME="cfrac"
+RUN_EXP="true"
+
+fi
+
+if [ "true" == "$RUN_EXP" ]; then
+
+if [ ! -d "$SUBJECT_NAME" ]; then
+    mkdir $SUBJECT_NAME 
+fi
+cp prepare.sh $SUBJECT_NAME 
+cd $SUBJECT_NAME 
+./prepare.sh $SUBJECT_NAME $SUBJECT_PATH_NAME
+
 cd $CURR_PATH
+rm -r $SUBJECT_NAME
+
+else
+    echo "Unknown experiment!"
+fi
+
+
+
+

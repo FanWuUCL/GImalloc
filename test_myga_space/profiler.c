bddc4551f11407dbbd53f8b81140e2de4ed9ff8d
FanWuUCL <fan.wu.12@ucl.ac.uk>
2014-02-12 20:29:00 +0000

    delete redundant code in profiler.c (test_myga_space)

--- test_myga_space/profiler.c
+++ test_myga_space/profiler.c
@@ -61,16 +61,13 @@ gint profile(double* time, double* memory, double* correctness, int suiteSize){
 			g_remove(filename);
 		}
 	}
-	gint redirectPipe[2];
 	line=g_malloc(512*sizeof(gchar));
 	gchar **cmdv=NULL;
 	gint pid, mpid, sampleN;
 	FILE *mfp;
-	memory_t=0;
 	time_t=0;
 	double tmp_double;
 	gint tmp_int;
-	pipe(redirectPipe);
 	struct rusage usage;
 	for(i=0; i<suiteSize; i++){
 		j=fullTest? i: randomIntRange(0, length);
@@ -93,90 +90,13 @@ gint profile(double* time, double* memory, double* correctness, int suiteSize){
 			exit(0);
 		}
 		g_strfreev(cmdv);
-		g_snprintf(filename, 128, "/proc/%d/smaps", pid);
-		if((mpid=fork())<0){
-			fprintf(stderr, "fork() for memory sampling failed.\n");
-			exit(0);
-		}
-		if(mpid==0){	// child process measuring the memory consumption
-			//close(redirectPipe[PIPE_IN]);
-			while(1){
-				if(!g_file_test(filename, G_FILE_TEST_EXISTS)){
-					//fprintf(logfp, "file %s not exist.\n", filename);
-					break;
-				}
-				tmp_double=0;
-
-				mfp=fopen(filename, "r");
-				if(!mfp) break;
-				while(!feof(mfp)){
-					if(!fgets(line, 256, mfp)){
-						break;
-					}
-					// search for writable mapping
-					tmp_int=0;
-					while(line[tmp_int]!=' ') tmp_int++;
-					if(line[tmp_int+2]=='w'){	// find a writable mapping
-						if(!g_str_has_suffix (line, "[heap]\n\0")){
-							continue;
-						}
-						// skip the next 5 lines
-						for(tmp_int=0; tmp_int<6; tmp_int++)
-							fgets(line, 256, mfp);
-						// add up private_dirty and private clean
-						sscanf(line, "Private_Clean: %d kB", &tmp_int);
-						//if(sampleN==0) fprintf(stderr, "%sread=%d\n", line, tmp_int);
-						tmp_double+=tmp_int;
-						fgets(line, 256, mfp);
-						sscanf(line, "Private_Dirty: %d kB", &tmp_int);
-						//if(sampleN==0) fprintf(stderr, "%sread=%d\n", line, tmp_int);
-						tmp_double+=tmp_int;
-						// skip the rest 8 lines
-						for(tmp_int=0; tmp_int<8; tmp_int++)
-							fgets(line, 256, mfp);
-						// should delete with if(suffix==[heap])
-						break;
-					}
-					else if(line[tmp_int+2]=='-'){	//	find a non-writable mapping, skip the next 15 lines
-						for(tmp_int=0; tmp_int<15; tmp_int++)
-							fgets(line, 256, mfp);
-					}
-				}
-				if(mfp) fclose(mfp);
-				if(tmp_double>memory_one_case) memory_one_case=tmp_double;
-
-				//g_snprintf(line, 512, "cp %s curr/smaps%d", filename, sampleN);
-				//usleep(1000);
-				sampleN++;
-			}
-			//if(profiler_debug) fprintf(stderr, "%d samples for case %d. memory= %lf\n", sampleN, j, memory_one_case);
-			g_snprintf(line, 128, "%lf\n", memory_one_case);
-			write(redirectPipe[PIPE_OUT], line, strlen(line));
-			//close(redirectPipe[PIPE_OUT]);
-			g_free(line);
-			g_free(append);
-			g_free(filename);
-			exit(0);
-		}
 
 		// parent process, wait for pid
 		wait4(pid, &tmp_int, 0, &usage);
-		tmp_int=read(redirectPipe[PIPE_IN], line, 512);
-		if(tmp_int<=0) fprintf(stderr, "read nothing from the pipe(%d). testcase %d.\n", tmp_int, i);
-		line[tmp_int]='\0';
-		sscanf(line, "%lf", &memory_one_case);
-		//g_spawn_close_pid(pid);
-		//fprintf(stderr, "%d samples for case %d.\n", sampleN, j);
 		memory_new_one=readProcMemory(j);
-		//fprintf(stderr, "heap=%d, for case %d.\n", memory_new_one, j);
 		memory_new+=memory_new_one;
-		memory_t+=memory_one_case;
 		time_t+=usage.ru_utime.tv_sec+usage.ru_stime.tv_sec+(usage.ru_utime.tv_usec+usage.ru_stime.tv_usec)/(double)1000000;
-		while(wait(&tmp_int)>0);
 	}
-	//freopen("/dev/tty", "w", stdout);
-	close(redirectPipe[PIPE_OUT]);
-	close(redirectPipe[PIPE_IN]);
 	g_free(line);
 	// cmp the output and summarise the correctness
 	correctness_t=0;
@@ -191,7 +111,6 @@ gint profile(double* time, double* memory, double* correctness, int suiteSize){
 	g_free(append);
 	if(profiler_debug) g_printf("memory: %lf\nmemory_new: %lf\ntime: %lf\n", memory_t, memory_new/(double)1024, time_t);
 	*time=time_t;
-	//*memory=memory_t;
 	*memory=memory_new/(double)1024;
 	//*correctness=correctness_t;
 	*correctness=0;

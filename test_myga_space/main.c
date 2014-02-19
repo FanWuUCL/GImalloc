4015fa317421cb5f3ce7627cd591f958bdc2717a
Yue Jia <yueplus@googlemail.com>
2014-02-19 21:27:41 +0000

    Add all testcases for space

--- test_myga_space/main.c
+++ test_myga_space/main.c
@@ -134,7 +134,7 @@ void main(int argc, char** argv){
 	printStdComsumption();
 	
         /* Random  */
-	population=initializeRandPopulation(ori, 1000);
+	population=initializeRandPopulation(ori, 100);
 
         /* NSGAII 
 	population=initializePopulation(ori, POPULATION_SIZE);

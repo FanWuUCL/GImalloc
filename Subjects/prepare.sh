eba8c5225949f87da932b1a6e77e9cf3ac9389ca
Yue Jia <yueplus@googlemail.com>
2014-02-20 21:24:39 +0000

    Set experiment for space

--- Subjects/prepare.sh
+++ Subjects/prepare.sh
@@ -6,6 +6,8 @@ if [ ! -d "$DIRECTORY" ]; then
     mkdir bin
 fi
 
+gcc -shared -fPIC malloc.c -o libmalloc.so
+
 # Prepare subject diff
 #echo "Prepare subject: Diff 3.2"
 #cd diffutils-3.2 
@@ -15,49 +17,49 @@ fi
 
 
 # Prepare subject fat_malloc 
-cd $curr_path
-echo "Prepare subject: fat_malloc"
-cd fat_memory
-make
-cp fat_memory ../bin
-cp libmalloc.so ../bin
+#cd $curr_path
+#echo "Prepare subject: fat_malloc"
+#cd fat_memory
+#make
+#cp fat_memory ../bin
+#cp libmalloc.so ../bin
 
 # Prepare subject ed 
-cd $curr_path
-echo "Prepare subject: ed"
-cd ed-1.9 
-./configure
-make
-cp ed ../bin
-cp libmalloc.so ../bin
+#cd $curr_path
+#echo "Prepare subject: ed"
+#cd ed-1.9 
+#./configure
+#make
+#cp ed ../bin
+#cp libmalloc.so ../bin
 
 
 # Prepare subject espresso 
-cd $curr_path
-echo "Prepare subject: Espresso"
-cd espresso-ab-1.0 
-./configure
-cd src
-make
-cp espresso ../../bin
+#cd $curr_path
+#echo "Prepare subject: Espresso"
+#cd espresso-ab-1.0 
+#./configure
+#cd src
+#make
+#cp espresso ../../bin
 #cp libmalloc.so ../bin
 
 
 # Prepare subject cfrac 
-cd $curr_path
-echo "Prepare subject: Cfrac"
-cd cfrac 
-make
-cp cfrac ../bin
+#cd $curr_path
+#echo "Prepare subject: Cfrac"
+#cd cfrac 
+#make
+#cp cfrac ../bin
 
 # Prepare subject gawk 
-cd $curr_path
-echo "Prepare subject: Gawk"
-cd gawk-4.0.2
-./configure
-cd src
-make
-cp gawk ../bin
+#cd $curr_path
+#echo "Prepare subject: Gawk"
+#cd gawk-4.0.2
+#./configure
+#cd src
+#make
+#cp gawk ../bin
 
 # Prepare subject space 
 cd $curr_path
@@ -67,11 +69,11 @@ make
 cp space ../bin
 
 # Prepare subject hashmap 
-cd $curr_path
-echo "Prepare subject: hashmap"
-cd c_hashmap 
-make
-cp hashmap ../bin
+#cd $curr_path
+#echo "Prepare subject: hashmap"
+#cd c_hashmap 
+#make
+#cp hashmap ../bin
 
 
 

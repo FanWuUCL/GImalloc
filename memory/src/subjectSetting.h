e74eebf84b4a5a519d953f1291903f0e292e9e2a
FanWuUCL <fan.wu.12@ucl.ac.uk>
2014-02-20 18:27:28 +0000

    minor change to correct the testcase path

--- memory/src/subjectSetting.h
+++ memory/src/subjectSetting.h
@@ -2,8 +2,8 @@
 #define SETTING_H
 
 // testcases dir, put in front of each test case and send it to the subject program in the cmd line
-#define DEFAULT_TESTCASES_DIR "testcases/"
-#define DEFAULT_CURR_DIR "curr/"
+#define DEFAULT_TESTCASES_DIR ""
+#define DEFAULT_CURR_DIR ""
 // additional compile command for malloc.c
 #define DEFAULT_COMPILE_CMD ""
 

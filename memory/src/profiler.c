7dee62752a4aacc3ba34340ca547e9e7303715bf
FanWuUCL <fan.wu.12@ucl.ac.uk>
2014-02-21 11:17:23 +0000

    minor change to profiler.c in memory/

--- memory/src/profiler.c
+++ memory/src/profiler.c
@@ -114,7 +114,10 @@ gint profile(double* time, double* memory, double* correctness, int suiteSize){
 	g_free(append);
 	//if(profiler_debug) g_printf("memory: %lf\ntime: %lf\n", memory_new/(double)1024, time_t);
         // YJ: remove value name
-	if(profiler_debug) g_printf("%lf\n%lf\n", memory_new/(double)1024, time_t);
+	if(profiler_debug){
+		fprintf(stdout, "%lf\n%lf\n", memory_new/(double)1024, time_t);
+		fflush(stdout);	
+	}
 	*time=time_t;
 	//*memory=memory_t;
 	*memory=memory_new/(double)1024;
